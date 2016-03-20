a9d61ea4837b40d2b629368d528c421d553e0229
Johan Lundin <johan@bluenine.se>
2016-03-20 15:31:44 +0000

    Adding extended latin

---
+++ test/latin1.js
@@ -0,0 +1,17 @@
+
+var testName = "Latin1";
+var offset = 0xA0;
+var end = 0x17f;
+
+var x = require('casper').selectXPath;
+var iterator = require('testiterator')();
+var utils = require("utils");
+
+casper.test.begin('Testing of character set', function(test) {
+
+    iterator.launch('http://localhost:3000/', test, offset);
+    casper.run(function() {
+        this.capture(testName + "done.png");
+        test.done();
+    });
+});
---
+++ test/latina.js
@@ -0,0 +1,17 @@
+
+var testName = "LatinA";
+var offset = 0x100;
+var end = 0x1ff;
+
+var x = require('casper').selectXPath;
+var iterator = require('testiterator')();
+var utils = require("utils");
+
+casper.test.begin('Testing of character set', function(test) {
+
+    iterator.launch('http://localhost:3000/', test, offset);
+    casper.run(function() {
+        this.capture(testName + "done.png");
+        test.done();
+    });
+});
