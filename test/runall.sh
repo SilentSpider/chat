54c9e1e47486597bc63001ef600acd3a14922d0a
Johan Lundin <johan@bluenine.se>
2016-03-20 16:51:50 +0000

    Adding extended latin

---
+++ test/runall.sh
@@ -0,0 +1,24 @@
+
+casperjs --xunit=simple.xml test simple.js
+casperjs --xunit=spaceinusername.xml test spaceinusername.js
+casperjs --xunit=longchat.xml test longchat.js
+casperjs --xunit=longchatreenter.xml test longchatreenter.js
+casperjs --xunit=ascii.xml test ascii.js
+casperjs --xunit=latin1.xml test latin1.js
+casperjs --xunit=latina.xml test latina.js
+casperjs --xunit=latinb.xml test latinb.js
+
+casperjs --xunit=diacritical.xml test diacritical.js
+casperjs --xunit=ipa.xml test ipa.js
+casperjs --xunit=space.xml test space.js
+casperjs --xunit=greekcoptic.xml test greekcoptic.js
+
+casperjs --xunit=arabic.xml test arabic.js
+casperjs --xunit=armenian.xml test armenian.js
+casperjs --xunit=cyrillic.xml test cyrillic.js
+casperjs --xunit=hebrew.xml test hebrew.js
+
+casperjs --xunit=sinhala.xml test sinhala.js
+casperjs --xunit=thai.xml test thai.js
+casperjs --xunit=lao.xml test lao.js
+casperjs --xunit=tibetan.xml test tibetan.js
